a6c12dd0a4f68a980164df25fc739d3d92fedc3e
roman lepihov <lepihovrom@yandex.by>
2024-05-31 09:56:07 +0300

    add hw3

---
+++ hw_3/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const viewCountsFileName = 'viewCounts';
+
+let viewCounts = {};
+try {
+  viewCounts = JSON.parse(fs.readFileSync(path.join(__dirname, viewCountsFileName), 'utf-8'));
+} catch (e) {
+
+}
+
+app.get('/', (req, res) => {
+  addViewCount(viewCounts, '/');
+  res.send(`
+<h1>Корневая страница</h1>
+<p>Просмотров: ${viewCounts['/']}</p>
+<a href="/about">Перейти на страницу /about</a>
+`)
+});
+app.get('/about', (req, res) => {
+  addViewCount(viewCounts, '/about');
+  res.send(`
+<h1>Страница About</h1>
+<p>Просмотров: ${viewCounts['/about']}</p>
+<a href="/">Перейти на главную страницу</a>
+`)
+});
+
+app.use(express.static('static_pages'));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'static_pages', '404.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Сервер запущен на порту 3000');
+});
+
+function addViewCount(viewCounts, url) {
+  if (viewCounts.hasOwnProperty(url)) {
+    viewCounts[url] += 1;
+  } else {
+    viewCounts[url] = 1;
+  }
+
+  fs.writeFileSync(path.join(__dirname, viewCountsFileName), JSON.stringify(viewCounts, null, 2));
+}
