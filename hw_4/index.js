d22f212341cfb1263942556111a3e2f657d5aaf0
roman lepihov <lepihovrom@yandex.by>
2024-06-04 19:17:15 +0300

    update hw_4

--- hw_4/index.js
+++ hw_4/index.js
@@ -3,26 +3,88 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const e = require("express");
+const joy = require("joy")
 const port = 3000;
 const usersFile = '/db/users.json'
 
 app.use(express.json())
+
+const usersSchema = joy.object({
+  name: joy.string().min(1).required(),
+  surname: joy.string().min(1).required(),
+  age: joy.number().min(0).required(),
+  city: joy.string(),
+})
 
 app.get('/users', (req, res) => {
   const users = JSON.parse(fs.readFileSync(path.join(__dirname, usersFile), 'utf-8'));
   res.send({users});
 });
 
-app.post('/', (req, res) => {
-
+app.get('/user/:id', (req, res) => {
+  const users = JSON.parse(fs.readFileSync(path.join(__dirname, usersFile), 'utf-8'));
+  const user = users.find((item) => {
+    item.id === +req.params.id
+  })
+  if (user) {
+    res.send({user});
+  } else {
+    res.status(404).send({user: null, error: "not found", status: "error"});
+  }
 });
 
-app.put('/', (req, res) => {
+app.post('/users', (req, res) => {
+  const result = usersSchema.validate(req.body)
+  if (result.error) {
+    return res.status(404).send({error: result.error.details, status: 'error'})
+  }
 
+  const users = JSON.parse(fs.readFileSync(path.join(__dirname, usersFile), 'utf-8'));
+  const user = {
+    name: req.body.name,
+    surname: req.body.surname,
+    age: req.body.age,
+    city: req.body.city,
+  }
+  users.push(user);
+  fs.writeFileSync(path.join(__dirname, usersFile), JSON.stringify(users, null, 2));
+  res.send({user});
 });
 
-app.delete('/', (req, res) => {
+app.put('/user/:id', (req, res) => {
+  const result = usersSchema.validate(req.body)
+  if (result.error) {
+    return res.status(404).send({error: result.error.details, status: 'error'})
+  }
 
+  const users = JSON.parse(fs.readFileSync(path.join(__dirname, usersFile), 'utf-8'));
+  const user = users.find((item) => {
+    item.id === +req.params.id
+  })
+  if (user) {
+    user.name = req.body.name;
+    user.surname = req.body.surname;
+    user.age = req.body.age;
+    user.city = req.body.city;
+    fs.writeFileSync(path.join(__dirname, usersFile), JSON.stringify(users, null, 2));
+    res.send({user});
+  } else {
+    res.status(404).send({user: null, error: "not found", status: "error"});
+  }
+});
+
+app.delete('/user/:id', (req, res) => {
+  const users = JSON.parse(fs.readFileSync(path.join(__dirname, usersFile), 'utf-8'));
+  const userIndex = users.findIndex((item) => {
+    item.id === +req.params.id
+  })
+  if (userIndex > -1) {
+    users.slice(userIndex, 1);
+    fs.writeFileSync(path.join(__dirname, usersFile), JSON.stringify(users, null, 2));
+    res.send({status: 'ok'});
+  } else {
+    res.status(404).send({user: null, error: "not found", status: "error"});
+  }
 });
 
 app.use(express.static('static_pages'));
